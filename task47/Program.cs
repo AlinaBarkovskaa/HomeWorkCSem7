8594b78a2a63767eabe6743ae5851a9bfa2257ac
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-12-12 15:30:21 +0400

    Added task 52

--- task47/Program.cs
+++ task47/Program.cs
@@ -1,17 +1,17 @@
-﻿  void InputmArray(double[,] matrix)
- {
-    for(int i = 0; i < matrix.GetLength(0); i++)
+﻿void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-       for(int j = 0; j < matrix.GetLength(1); j++)
-       {
-          matrix[i, j] = new Random().Next(-100, 100)/10.0;
-           Console.Write($"{matrix[i, j]} \t");
-       }
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11); 
+            Console.Write($"{matrix[i, j]} \t");
+        }
         Console.WriteLine();
     }
- } 
- Console.Clear();
-Console.WriteLine("Введите размер матрицы: ");
- string[] numbers = Console.ReadLine().Split(" ");
- double[,] matrix = new double[int.Parse(numbers[0]), int.Parse(numbers[1])];
- InputmArray(matrix);
+}
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+double[,] matrix = new double[int.Parse(numbers[0]), int.Parse(numbers[1])];
+InputMatrix(matrix);
---
+++ task52/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+
+void InputmArray(int[,] matrix)
+ {
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+       for(int j = 0; j < matrix.GetLength(1); j++)
+       {
+          matrix[i, j] = new Random().Next(0, 10);
+           Console.Write($"{matrix[i, j]} \t");
+       }
+        Console.WriteLine();
+    }
+ } 
+ void arithmeticMeanElements(int[,] matrix)
+ {
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+       int avarage = 0;
+       for(int i = 0; i < matrix.GetLength(0); i++)
+   {
+       avarage = (avarage + matrix[i, j]);
+   }
+    avarage = avarage / n;
+    Console.WriteLine($"среднее арифметическое элементов в столбце = {avarage} ");
+    } 
+ }
+Console.Clear();
+InputmArray(matrix);
+arithmeticMeanElements(matrix);
